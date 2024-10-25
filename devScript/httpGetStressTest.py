feb088b1bd88444aec71f3ce74a0ab78a0808396
matteoVtopia <matteo@vtopia-vr.com>
2024-10-25 13:48:21 +0200

    Startup setted, and http stress test

---
+++ devScript/httpGetStressTest.py
@@ -0,0 +1,21 @@
+import requests
+import time
+
+def send_http_get(url, interval):
+    
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            print("HTTP GET request sent successfully")
+        else:
+            print(f"Failed to send HTTP GET request. Status code: {response.status_code}")
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred while sending the HTTP GET request: {e} at time {time.time().strftime('%Y-%m-%d %H:%M:%S')}")
+    
+
+if __name__ == "__main__":
+    url = "http://172.16.5.54:8080/recordings"
+    interval = 60  # Replace X with the desired interval in seconds
+    while True:
+        send_http_get(url, interval)
+        time.sleep(interval)
